0e4d5dd65198cdbc9961efc85470e9b4b27ff25f
jcherven <cherven.james@gmail.com>
2020-06-13 15:21:06 -0400

    handles form submit with post request

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <div>
-      <form action>
-        <input placeholder="email" />
-        <input placeholder="password" />
-        <input placeholder="password confirmation" />
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
         <button>Sign Up</button>
       </form>
     </div>
   `);
 });
 
+app.post("/", (req, res) => {
+  res.send("congrats on your new fake account");
+});
+
 app.listen(3000, () => {
-  console.log('Listening on 3000');
-})
+  console.log("Listening on 3000");
+});
