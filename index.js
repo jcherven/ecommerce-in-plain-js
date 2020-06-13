6eac62d0a32265f4d1063004e861aa60edcb4fe0
jcherven <cherven.james@gmail.com>
2020-06-13 15:10:52 -0400

    adds a lil sign in form

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("i swear i'm trying to help but i'm forced to make web apps");
+  res.send(`
+    <div>
+      <form action>
+        <input placeholder="email" />
+        <input placeholder="password" />
+        <input placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
 });
 
 app.listen(3000, () => {
-  console.log('Listening');
+  console.log('Listening on 3000');
 })
